f6159adc627f64c2c6e6aebee8b91162c7f9eb8e
Alex Pieschel <apieschel@gmail.com>
2022-07-19 09:15:18 -0500

    set up assets, added main layout to portfolio page

--- _data/posts.js
+++ _data/posts.js
@@ -3,8 +3,8 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function() {
-  return EleventyFetch( "https://dev-gtr-software-api.pantheonsite.io/wp-json/wp/v2/posts", {
-    duration: "1d",
+  return EleventyFetch( "https://dev-gtr-software-api.pantheonsite.io/wp-json/wp/v2/posts?_embed", {
+    duration: "30s",
     type: "json"   
   });
 };
